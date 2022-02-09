f7ebbd55755fcaf2038b8405d002ec5df1845f12
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2022-02-09 10:45:22 -0500

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+import 'dotenv/config'
+import express from 'express'
+import notes from './notes'
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.post('/api/notes', async (request, response ) => {
+const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id)) 
+    : 0
+
+  const note = request.body
+  note.id = maxId + 1
+
+  notes = notes.concat(note)
+
+  response.json(note)
+
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
