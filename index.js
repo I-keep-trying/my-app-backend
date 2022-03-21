8e0f25365555f0e1f9c01f58fae9d883103d0408
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2022-03-21 14:50:52 -0400

    update csp

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use(
     directives: {
       'img-src': [
         "'self'",
-        "'data'",
-        'blob:',
+       /*  "'data'", */
+       'data:'
         'flagcdn.com',
         'upload.wikimedia.org',
         'openweathermap.org',
@@ -26,6 +26,7 @@ app.use(
         'sheltered-scrubland-08732.herokuapp.com',
         'https://*.here.com:*',
         'https://*.hereapi.com:*',
+        "'blob'"
       ],
       /*    'style-src': [
         "'self'",
