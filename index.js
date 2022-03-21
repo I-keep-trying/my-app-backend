bff5c22850b2e2e3ec82640691ba73519597fe68
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2022-03-21 13:25:44 -0400

    update csp

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
       ],
       'script-src': ['self', 'sheltered-scrubland-08732.herokuapp.com'],
       'connect-src': [
-        'https://sheltered-scrubland-08732.herokuapp.com/*',
+        'sheltered-scrubland-08732.herokuapp.com',
         'https://*.here.com:*',
         'https://*.hereapi.com:*',
       ],
