1f651c0e9e08e07553a0c3f3f8c606aebd90122d
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2022-03-22 13:36:28 -0400

    csp config

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ app.use(
       ],
       'object-src': ["'none'"],
       'base-uri': ["'none'"],
+      'connect-src': ["'https://sheltered-scrubland-08732.herokuapp.com'"],
+      'manifest-src': ["'https://sheltered-scrubland-08732.herokuapp.com'"],
       //  'require-trusted-types-for': [`'script'`], // cannot use. 'script' value requires further specifications which are a mystery to solve some other time.
     },
   })
