efb56ba09148facee0a1cbd88e794c2c394a6c37
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2022-04-11 17:09:29 -0400

    many improvements

--- controllers/countries.js
+++ controllers/countries.js
@@ -1,5 +1,12 @@
 const countriesRouter = require('express').Router()
 
 countriesRouter.get('/', async (req, res) => {
-  res.json(res.data)
+  try {
+    const response = await axios.get('https://restcountries.com/v3.1/all')
+
+    res.json(response.data)
+  } catch (err) {
+    console.log('axios request failed api/countries', err)
+    res.status(err.response.status).send(err.response.statusText)
+  }
 })
